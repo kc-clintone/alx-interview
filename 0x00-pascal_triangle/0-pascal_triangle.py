98e83122d7899b4ec3672ae6af3ad72b3a41fcd6
kc-clintone <kayseeclintone@gmail.com>
2024-07-31 21:19:16 +0000

    setup and first task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script prints pascal's triangle for a given number
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers representing the Pascal’s triangle
+    """
+    triangle = []
+
+    if num <= 0:
+        return triangle
+    for i in range(num):
+        tmp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                tmp_list.append(1)
+            else:
+                tmp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(tmp_list)
+    return triangle
