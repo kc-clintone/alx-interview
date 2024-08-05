eec87f36cc5fe8bfd6618cec950a101a41ab4c7e
kc-clintone <kayseeclintone@gmail.com>
2024-08-05 07:43:16 +0000

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Calculate the minimum number of operations required for
+Copy and Paste ops on a file
+"""
+
+
+def minOperations(n):
+    """
+    This function calculates the number of operations
+    required for a task
+    """
+    if n == 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
+
